f958d319b388c7dfc8bf7572534b3d92cf6357d4
enabledornot <102371948+enabledornot@users.noreply.github.com>
2024-02-25 21:30:37 -0500

    added more incomplete code

--- binary_search_avl/src/main.rs
+++ binary_search_avl/src/main.rs
@@ -1,5 +1,23 @@
 use rand::{Rng, thread_rng};
 
+struct Bst<T: PartialOrd> {
+    base: Option<&Bst_node>,
+}
+struct Bst_node<T: PartialOrd> {
+    right: Option<&Bst_node>,
+    left: Option<&Bst_node>,
+    data: &T
+}
+impl<T: PartialOrd> Bst<T: PartialOrd> {
+    fn new() -> Self {
+        Bst {
+            base: None,
+        }
+    }
+    fn insert() {
+
+    }
+}
 fn rand() -> u32 {
     let rnd: i32 = thread_rng().gen_range(0..1000);
     return rnd as u32;
