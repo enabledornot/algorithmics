5d918774aecbb34c1ede5e28948cde09b56e2b9a
enabledornot <102371948+enabledornot@users.noreply.github.com>
2024-03-03 20:50:01 -0500

    completed shift by block

--- homework_2/left_shift/src/main.rs
+++ homework_2/left_shift/src/main.rs
@@ -6,7 +6,33 @@ fn generate_seq_ary(length: i32) -> Vec<i32> {
     return rslt;
 }
 
+fn req_swap(ary: &mut [i32], k: usize) -> usize{
+    let mut i = 0;
+    let mut j = k;
+    while i<k && j < ary.len() {
+        ary.swap(i,j);
+        i = i + 1;
+        j = j + 1;
+    }
+    return i;
+}
+
+fn shift_n(ary: &mut [i32], amnt: usize) {
+    let c = req_swap(ary, amnt);
+    if c == 0 {
+        return
+    }
+    else if c < amnt {
+        return shift_n(&mut ary[c..],amnt-c);
+    }
+    else {
+        return shift_n(&mut ary[c..],amnt);
+    }
+
+}
+
 fn main() {
-    let seq_arry = generate_seq_ary(10);
-    print!("{:?}",seq_arry);
+    let mut seq_arry = generate_seq_ary(14);
+    shift_n(&mut seq_arry,12);
+    println!("{:?}",seq_arry);
 }
