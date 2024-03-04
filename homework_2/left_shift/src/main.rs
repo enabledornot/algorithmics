4393e671712a8a7b7ab4158d42855177c2920482
enabledornot <102371948+enabledornot@users.noreply.github.com>
2024-03-03 21:11:39 -0500

    added shuffle algorithm and fixed issue in divide and conquer algorithm

--- homework_2/left_shift/src/main.rs
+++ homework_2/left_shift/src/main.rs
@@ -18,25 +18,55 @@ fn req_swap(ary: &mut [i32], k: usize) -> usize{
     }
     return i;
 }
-
 fn shift_n_divide(ary: &mut [i32], amnt: usize) {
+    let len = ary.len();
+    shift_n_divide_rec(ary, len-amnt);
+}
+fn shift_n_divide_rec(ary: &mut [i32], amnt: usize) {
     let c = req_swap(ary, amnt);
     if c == 0 {
         return
     }
     else if c < amnt {
-        return shift_n(&mut ary[c..],amnt-c);
+        return shift_n_divide_rec(&mut ary[c..],amnt-c);
     }
     else {
-        return shift_n(&mut ary[c..],amnt);
+        return shift_n_divide_rec(&mut ary[c..],amnt);
     }
 
 }
 
+// shuffle
+fn gcd(m: usize, n: usize) -> usize {
+    if n == 0 {
+        return m;
+    }
+    return gcd(n, m % n);
+}
 
+fn shift_n_shuffle(ary: &mut [i32], amnt: usize) {
+    let len = ary.len();
+    for i in 0..gcd(len,amnt) {
+        let mut cnt = (i+amnt)%len;
+        let mut last = ary[i];
+        while cnt != i {
+            let lastalt = ary[cnt];
+            ary[cnt] = last;
+            last = lastalt;
+            cnt = (cnt+amnt)%len;
+        }
+        ary[i] = last;
+    }
+}
 
 fn main() {
+    println!("By divide and conquer");
     let mut seq_arry = generate_seq_ary(14);
-    shift_n(&mut seq_arry,12);
+    shift_n_divide(&mut seq_arry,12);
+    println!("{:?}",seq_arry);
+
+    println!("By shuffle method");
+    seq_arry = generate_seq_ary(14);
+    shift_n_shuffle(&mut seq_arry,12);
     println!("{:?}",seq_arry);
 }
