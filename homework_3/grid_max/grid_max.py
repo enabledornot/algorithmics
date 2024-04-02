83f78e7e603324b77b15396825c1221968a62b42
enabledornot <102371948+enabledornot@users.noreply.github.com>
2024-04-02 12:12:44 -0400

    added a cache

--- homework_3/grid_max/grid_max.py
+++ homework_3/grid_max/grid_max.py
@@ -1,6 +1,6 @@
 import random
 import ipdb
-# helper
+from tqdm import tqdm
 def rand_int_ary(n):
     ary = []
     for i in range(n):
@@ -80,7 +80,7 @@ def dijkstra_multi(mask,bound,n_chunks):
                 pdist = weights[c[0]][c[1]] + (-1*mask[p[0]][p[1]])
                 if pdist < weights[p[0]][p[1]] or weights[p[0]][p[1]] == -1:
                     weights[p[0]][p[1]] = pdist
-    print_ary(weights)
+    # print_ary(weights)
     return min_dists
 def find_min_and_remove(to_explore,mask):
     min = 1000000000000
@@ -102,6 +102,9 @@ class most_efficient_path:
         self.edge = edge_weights
         self.node = node_weights
         self.n = len(node_weights)
+        self.cache = []
+        for v in range(self.n):
+            self.cache.append({})
     def price_between(self, node_a, node_b):
         if node_a == node_b:
             return 0
@@ -110,28 +113,29 @@ class most_efficient_path:
             node_a = node_b
             node_b = node_t
         res = self.edge[node_b][node_a]
-        if res == -1:
-            asdf
         return res
     def most_efficient_start(self):
         possible = [0]
-        for n in range(1,self.n):
-            potential = self.most_efficient(n,[n])
+        for n in tqdm(range(1,self.n)):
+            potential = self.most_efficient_c(n,{n})
             if potential != None:
                 possible.append(potential + self.node[n])
-        print(possible)
         return max(possible)
+    def most_efficient_c(self, current, prev):
+        if str(prev) not in self.cache[current]:
+            self.cache[current][str(prev)] = self.most_efficient(current, prev)
+        return self.cache[current][str(prev)]
     def most_efficient(self, current, prev):
         possible = [0]
         for v in range(1,self.n):
             if v not in prev:
-                potential = self.most_efficient(v,prev + [v])
+                potential = self.most_efficient_c(v,prev | {v})
                 if potential != None:
                     possible.append(potential + self.node[v] - self.price_between(current, v))
         return max(possible)
 
-iary = rand_int_ary(4)
-iary = [[-18, 73, 10, -8], [76, 67, 25, 11], [-42, -30, -56, -5], [83, -86, 1, -19]]
+iary = rand_int_ary(10)
+# iary = [[-18, 73, 10, -8], [76, 67, 25, 11], [-42, -30, -56, -5], [83, -86, 1, -19]]
 print(iary)
 print_ary(iary)
 map_array, node_weights = divide_up(iary)
@@ -142,7 +146,5 @@ edge_weights = find_distances(map_array,node_weights)
 print_ary(edge_weights)
 
 mfp = most_efficient_path(node_weights,edge_weights)
-# ipdb.set_trace()
-# result = mfp.most_efficient(1,[1])
 result = mfp.most_efficient_start()
 print(result)
