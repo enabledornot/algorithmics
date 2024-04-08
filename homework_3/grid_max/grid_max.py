2c4cb28aa79e3de0cc6a0fa460fbfa9b71b5e58f
enabledornot <102371948+enabledornot@users.noreply.github.com>
2024-04-08 11:49:37 -0400

    added updated grid_code

--- homework_3/grid_max/grid_max.py
+++ homework_3/grid_max/grid_max.py
@@ -1,6 +1,8 @@
 import random
 # import ipdb
 from tqdm import tqdm
+import numpy as np
+import functools
 def rand_int_ary(n):
     ary = []
     for i in range(n):
@@ -101,23 +103,36 @@ class cache:
     def __init__(self,n):
         self.n = n
         self.c = self.get_none()
+        self.hitcount = 0
+        self.lookcount = 0
     def get_none(self):
         return [None]*self.n
     def lookup(self, k, method, arg):
+        self.lookcount += 1
         # print(method(arg))
         k = sorted(list(k))
         current = self.c
-        for p in k[:-1]:
+        for p in k:
             if current[p] == None:
                 current[p] = self.get_none()
             current = current[p]
-        if current[k[-1]] == None:
-            current[k[-1]] = method(arg)
-        return current[k[-1]]
+        if current[0] == None:
+            current[0] = method(arg)
+        else:
+            self.hitcount += 1
+        return current[0]
     def hash(k):
         return str(k)
+    def size(c):
+        count = 0
+        for i in c:
+            if type(i) == list:
+                count += cache.size(i)
+            elif i != None:
+                count += 1
+        return count
     def len(self):
-        return len(self.c)
+        return cache.size(self.c)
 class most_efficient_path:
     def __init__(self, node_weights, edge_weights):
         self.edge = edge_weights
@@ -137,19 +152,16 @@ class most_efficient_path:
         return res
     def most_efficient_start(self):
         possible = [0]
+        new_array = tuple([False]*self.n)
         for n in tqdm(range(1,self.n)):
-            potential = self.most_efficient_c({n})
+            nau = most_efficient_path.update_tuple(new_array,n)
+            potential = self.most_efficient_path(nau)
             if potential != None:
                 possible.append(potential + self.node[n])
         return max(possible)
     def most_efficient_c(self, prev):
-        # if (result := self.cache.lookup(prev, self.most_efficient, prev)):
-        #     return result
-        # result = self.most_efficient(prev)
-        # self.cache.add(prev, result)
         return self.cache.lookup(prev, self.most_efficient, prev)
     def most_efficient(self, prev):
-        print("prev")
         possible = [0]
         for s in range(1,self.n):
             for e in range(1,self.n):
@@ -157,8 +169,20 @@ class most_efficient_path:
                     if (price := self.price_between(s, e)) and (potential := self.most_efficient_c(prev | {e}))==0:
                         possible.append(potential + self.node[e] - price)
         return max(possible)
-# iary = rand_int_ary(12)
-iary = [[-18, 73, 10, -8], [76, 67, 25, 11], [-42, -30, -56, -5], [83, -86, 1, -19]]
+    @functools.cache
+    def most_efficient_path(self, prev):
+        possible = [0]
+        for s in range(1,self.n):
+            for e in range(1,self.n):
+                if prev[s] and not prev[e] and (price := self.price_between(s, e)) and (potential := self.most_efficient_path(most_efficient_path.update_tuple(prev,e)))==0:
+                    possible.append(potential + self.node[e] - price)
+        return max(possible)
+    def update_tuple(tup, i):
+        n = list(tup)
+        n[i] = True
+        return tuple(n)
+iary = rand_int_ary(20)
+# iary = [[-18, 73, 10, -8], [76, 67, 25, 11], [-42, -30, -56, -5], [83, -86, 1, -19]]
 # iary = [[22, -60, -50, 18, 26, -62, 61, 72, -33, -91, -42, 100], [-13, 24, 73, 93, -80, -95, -46, 12, 84, -14, 85, -56], [27, 73, -53, -7, -84, 80, 42, 80, -91, -28, -50, 92], [-60, -81, -21, 15, -4, 2, -100, -84, -12, -7, -83, 1], [-69, 85, 41, -71, -85, -93, 28, 90, 84, -91, -54, -50], [27, 66, 44, 8, -66, -81, -13, -75, 96, -97, 79, -69], [-50, -62, 70, 25, -98, 17, 96, -46, -1, 69, -25, 52], [-85, 91, -86, 87, 40, -98, -37, 95, 85, -69, -86, 23], [-64, -27, -99, -38, 2, 15, -48, -77, 6, 51, -69, -65], [95, 4, 59, 6, 1, -67, -51, 38, 2, -33, -14, -5], [45, 91, 11, 13, -45, -31, 6, -3, 47, 76, 59, 75], [-4, -22, 70, -75, -55, -54, 24, -50, -56, 40, -99, -29]]
 print(iary)
 print_ary(iary)
@@ -173,3 +197,5 @@ mfp = most_efficient_path(node_weights,edge_weights)
 result = mfp.most_efficient_start()
 print(mfp.cache.len())
 print(result)
+# print(mfp.cache.hitcount)
+# print(mfp.cache.lookcount)
