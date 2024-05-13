b988aeb8f0197f48a0dcf505612d697556ee36f6
enabledornot <102371948+enabledornot@users.noreply.github.com>
2024-05-13 15:26:25 -0400

    added improved prime siv

---
+++ homework_5/prime/prime_siv2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define UPPER_BOUND 1000000
+
+void siv_up(_Bool* array_map, long start_index) {
+    int jump = start_index * 2 + 1;
+    for(long i = start_index + jump; i < UPPER_BOUND / 2; i+= jump) {
+        array_map[i] = 1;
+    }
+}
+
+void prime_siv(_Bool* array_map) {
+    array_map[0] = 1;
+    array_map[1] = 1;
+    for(long i = 2; i< UPPER_BOUND / 2; i++) {
+        if(array_map[i] == 0) {
+            siv_up(array_map, i);
+        }
+    }
+}
+
+void calculate_primes() {
+    _Bool array_map[UPPER_BOUND / 2] = {0};
+    prime_siv(array_map);
+    long i = (UPPER_BOUND/2)-1;
+    while(array_map[i] == 1) {
+        i = i - 1;
+    }
+    printf("%ld\n",i*2 + 1);
+}
+
+int main(int argc, char *argv[]) {
+    for(int i = 0;i<100;i++) {
+        calculate_primes();
+    }
+}
