5df57b9e561a3c7fb7747b01171c0388768f7e13
enabledornot <102371948+enabledornot@users.noreply.github.com>
2024-05-13 18:38:13 -0400

    modified prime siv dev program to support adding 2 special_sets

--- homework_5/prime/prime_siv30.py
+++ homework_5/prime/prime_siv30.py
@@ -12,31 +12,44 @@ class special_set:
         else:
             raise ValueError("starting value does not exist within the set")
     def __add__(self,other):
-        if not isinstance(other, int):
-            raise TypeError("unsupported addition type {}".format(type(isinstance)))
-        new_offset = magic_array[self.bit] + other
-        while new_offset % 30 not in magic_array:
-            new_offset += other
-        return special_set((self.byte + int(new_offset / 30), magic_array.index(new_offset % 30)))
+        if isinstance(other, special_set):
+            other = 30*other.byte + other.bit
+        if isinstance(other, int):
+            new_offset = magic_array[self.bit] + other
+            while new_offset % 30 not in magic_array:
+                new_offset += other
+            return special_set((self.byte + int(new_offset / 30), magic_array.index(new_offset % 30)))
+        else:
+            raise TypeError("unsupported addition type {}".format(type(other)))
     def add_lookup(self,other):
-        if not isinstance(other, int):
-            raise TypeError("unsupported addition type {}".format(type(isinstance)))
-        mod30 = other % 30
-        div30 = int(other / 30)
-        looked_up = lookup[self.bit][mod30]
-        
-        byte_offset = div30 * looked_up[2] + looked_up[0]
-        new_bit = looked_up[1]
-        return special_set((self.byte + byte_offset, new_bit))
+        if isinstance(other, special_set):
+            looked_up = lookup[self.bit][other.bit]
+
+            byte_offset = other.byte * looked_up[2] + looked_up[0]
+            new_bit = looked_up[1]
+            return special_set((self.byte + byte_offset, new_bit))
+        elif isinstance(other, int):
+            mod30 = other % 30
+            div30 = int(other / 30)
+            looked_up = lookup[self.bit][mod30]
+            
+            byte_offset = div30 * looked_up[2] + looked_up[0]
+            new_bit = looked_up[1]
+            return special_set((self.byte + byte_offset, new_bit))
+        else:
+            raise TypeError("unsupported addition type {}".format(type(other)))
     def get_add_offset(self,other):
-        if not isinstance(other, int):
-            raise TypeError("unsupported addition type {}".format(type(isinstance)))
-        new_offset = magic_array[self.bit] + other
-        count = 1
-        while new_offset % 30 not in magic_array:
-            count += 1
-            new_offset += other
-        return (int(new_offset / 30), magic_array.index(new_offset % 30),count)
+        if isinstance(other, special_set):
+            other = 30*other.byte + other.bit
+        if isinstance(other, int):
+            new_offset = magic_array[self.bit] + other
+            count = 1
+            while new_offset % 30 not in magic_array:
+                new_offset += other
+                count += 1
+            return (int(new_offset / 30), magic_array.index(new_offset % 30),count)
+        else:
+            raise TypeError("unsupported addition type {}".format(type(other)))
     def __str__(self):
         return str(30 * self.byte + magic_array[self.bit])
     def __eq__(self,other):
@@ -46,7 +59,7 @@ def generate_lookup_table():
     lookup_table = []
     for i in range(8):
         lookup_table.append([])
-        for ii in range(30):
+        for ii in range(8):
             ss = special_set((0,i))
             lookup_table[-1].append(ss.get_add_offset(ii))
     return lookup_table
@@ -61,6 +74,8 @@ def rand_ss(uppr):
 
 for _ in range(10000):
     ss = rand_ss(1000)
-    add = random.randint(1,1000)
-    if (ss+add != ss.add_lookup(add)):
-        print("{}-{}".format(ss,add))
+    ss2 = rand_ss(1000)
+    if (ss+ss2 != ss.add_lookup(ss2)):
+        print("{}-{}".format(ss,ss2))
+        print("{}-{}".format(ss+ss2,ss.add_lookup(ss2)))
+        print("")
