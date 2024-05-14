2d3a61f87be829e40a723e397a194522f66e1d49
enabledornot <102371948+enabledornot@users.noreply.github.com>
2024-05-13 20:51:43 -0400

    finished up v30 prime siv

--- homework_5/prime/prime_siv30.c
+++ homework_5/prime/prime_siv30.c
@@ -3,7 +3,10 @@
 #include <stdbool.h>
 
 #define UPPER_BOUND 1000000
+// Upper_Bound * (8/30)
+#define UPPER_BOUND_ARY 33334
 const int MAGIC_ARRAY[] = {1,7,11,13,17,19,23,29};
+const unsigned char BITMASK[] = {128,64,32,16,8,4,2,1};
 
 
 typedef struct bitmap_index {
@@ -16,7 +19,7 @@ typedef struct lookup_add {
     long reset_count;
 } l_a;
 
-l_a lookup[8][30];
+l_a lookup[8][8];
 
 bool in_magic(long v) {
     for(int i = 0;i < 8;i++) {
@@ -32,49 +35,124 @@ short in_magic_i(long v) {
     *((int*)0) = 42;
 }
 
-void get_add_offset(l_a* add_offset ,short bit, long offset) {
-    long new_offset = MAGIC_ARRAY[bit] + offset;
+long to_long(b_i* bitx) {
+    return bitx->byte*30 + MAGIC_ARRAY[bitx->bit];
+}
+
+void get_add_offset(l_a* add_offset ,short bit, short offset) {
+    long new_offset = MAGIC_ARRAY[bit] + MAGIC_ARRAY[offset];
     long count = 1;
     while(!in_magic(new_offset % 30)) {
+        // printf("%ld\n",new_offset);
         count += 1;
-        new_offset += offset;
+        new_offset += MAGIC_ARRAY[offset];
     }
     add_offset->byte_offset = new_offset / 30;
     add_offset->new_bit = in_magic_i(new_offset % 30);
     add_offset->reset_count = count;
 }
 
-void add_lookup(b_i* bindex, long v) {
-    l_a* looked_up = &lookup[bindex->bit][v % 30];
-    bindex->byte += (v / 30) * looked_up->reset_count + looked_up->byte_offset;
+void add_lookup(b_i* bindex, b_i* v) {
+    l_a* looked_up = &lookup[bindex->bit][v->bit];
+    bindex->byte += ((v->byte) * looked_up->reset_count) + looked_up->byte_offset;
     bindex->bit = looked_up->new_bit;
+}
+
+void itr_lookup(b_i* bindex) {
+    if (bindex->bit == 7) {
+        bindex->bit = 0;
+        bindex->byte += 1;
+    }
+    else {
+        bindex->bit += 1;
+    }
+}
+
+void detr_lookup(b_i* bindex) {
+    if (bindex->bit == 0) {
+        bindex->bit = 7;
+        bindex->byte -= 1;
+    }
+    else {
+        bindex->bit -= 1;
+    }
 }
 
 void buildLookup() {
     for(short bit = 0;bit<8;bit++) {
-        lookup[bit][0].byte_offset = 0;
-        lookup[bit][0].new_bit = bit;
-        lookup[bit][0].reset_count = 1;
-        for(long offset = 1;offset<30;offset++) {
+        // lookup[bit][0].byte_offset = 0;
+        // lookup[bit][0].new_bit = bit;
+        // lookup[bit][0].reset_count = 1;
+        for(long offset = 0;offset<8;offset++) {
             get_add_offset(&lookup[bit][offset],bit,offset);
         }
     }
-    for(short bit = 0;bit<8;bit++) {
-        for(long offset = 0;offset<30;offset++) {
-            printf("byte_offset = %ld, new_bit = %d, reset_count = %ld\n",lookup[bit][offset].byte_offset,lookup[bit][offset].new_bit,lookup[bit][offset].reset_count);
-        }
+    // for(short bit = 0;bit<8;bit++) {
+    //     printf("%d\n",bit);
+    //     for(long offset = 0;offset<8;offset++) {
+    //         printf("%ld: byte_offset = %ld, new_bit = %d, reset_count = %ld\n",offset,lookup[bit][offset].byte_offset,lookup[bit][offset].new_bit,lookup[bit][offset].reset_count);
+    //     }
+    //     printf("\n");
+    // }
+}
+
+void siv_up(unsigned char* array_map, b_i* start) {
+    b_i current = *start;
+    add_lookup(&current, start);
+    while(current.byte < UPPER_BOUND_ARY) {
+        // printf("%ld\n",current.byte*30 + MAGIC_ARRAY[current.bit]);
+        array_map[current.byte] = array_map[current.byte] | BITMASK[current.bit];
+        add_lookup(&current, start);
     }
 }
 
-int calculate_primes() {
-    _Bool array_map[UPPER_BOUND/8] = {0};
-    long i = UPPER_BOUND-1;
-    printf("%ld\n",i);
+void calculate_primes() {
+    buildLookup();
+    unsigned char array_map[UPPER_BOUND_ARY] = {0};
+    array_map[0] = array_map[0] + BITMASK[0];
+    b_i current = {0, 1};
+    while(current.byte < UPPER_BOUND_ARY) {
+        if(!(array_map[current.byte] & BITMASK[current.bit])) {
+            siv_up(array_map, &current);
+            // break;
+        }
+        itr_lookup(&current);
+    }
+    while(to_long(&current) > UPPER_BOUND) {
+        detr_lookup(&current);
+    }
+    while((array_map[current.byte] & BITMASK[current.bit])) {
+        detr_lookup(&current);
+    }
+    printf("%ld\n",to_long(&current));
+    // for(long i = 0;i<100;i++) {
+    //     printf("%d\n",array_map[i]);
+    // }
+    // for(long i = 0;i<UPPER_BOUND_ARY;i++) {
+    //     for(short ii = 0;ii<8;ii++) {
+    //         // if(true) {
+    //         if(!(array_map[i] & BITMASK[ii])) {
+    //             printf("%ld ",i*30 + MAGIC_ARRAY[ii]);
+    //         }
+    //     }
+    // }
+    // printf("\n");
+    // long i = UPPER_BOUND-1;
+    // printf("%ld\n",i);
 
 }
 
 int main(int argc, char *argv[]) {
-    buildLookup();
+    // buildLookup();
+    for(int i = 0;i<100;i++) {
+        calculate_primes();
+    }
+    // b_i current = {0, 1};
+    // b_i add = {0, 1};
+    // printf("%ld\n",to_long(&current));
+    // printf("%ld\n",to_long(&add));
+    // add_lookup(&current,&add);
+    // printf("%ld\n",to_long(&current));
     // printf("%d\n",in_magic_i(17));
     // for(int i = 0;i<100;i++) {
     //     calculate_primes();
--- homework_5/prime/prime_siv30.py
+++ homework_5/prime/prime_siv30.py
@@ -13,7 +13,7 @@ class special_set:
             raise ValueError("starting value does not exist within the set")
     def __add__(self,other):
         if isinstance(other, special_set):
-            other = 30*other.byte + other.bit
+            other = 30*other.byte + magic_array[other.bit]
         if isinstance(other, int):
             new_offset = magic_array[self.bit] + other
             while new_offset % 30 not in magic_array:
@@ -24,6 +24,7 @@ class special_set:
     def add_lookup(self,other):
         if isinstance(other, special_set):
             looked_up = lookup[self.bit][other.bit]
+            print(looked_up)
 
             byte_offset = other.byte * looked_up[2] + looked_up[0]
             new_bit = looked_up[1]
@@ -40,7 +41,7 @@ class special_set:
             raise TypeError("unsupported addition type {}".format(type(other)))
     def get_add_offset(self,other):
         if isinstance(other, special_set):
-            other = 30*other.byte + other.bit
+            other = 30*other.byte + magic_array[other.bit]
         if isinstance(other, int):
             new_offset = magic_array[self.bit] + other
             count = 1
@@ -61,7 +62,7 @@ def generate_lookup_table():
         lookup_table.append([])
         for ii in range(8):
             ss = special_set((0,i))
-            lookup_table[-1].append(ss.get_add_offset(ii))
+            lookup_table[-1].append(ss.get_add_offset(special_set((0,ii))))
     return lookup_table
 lookup = generate_lookup_table()
 
@@ -71,11 +72,19 @@ def rand_ss(uppr):
             return special_set(random.randint(1,uppr))
         except:
             pass
+import json
+with open("output.json","w") as f:
+    json.dump(lookup,f,indent=4)
+# for _ in range(10000):
+#     ss = rand_ss(1000)
+#     ss2 = rand_ss(1000)
+#     if (ss+ss2 != ss.add_lookup(ss2)):
+#         print("{}-{}".format(ss,ss2))
+#         print("{}-{}".format(ss+ss2,ss.add_lookup(ss2)))
+#         print("")
 
-for _ in range(10000):
-    ss = rand_ss(1000)
-    ss2 = rand_ss(1000)
-    if (ss+ss2 != ss.add_lookup(ss2)):
-        print("{}-{}".format(ss,ss2))
-        print("{}-{}".format(ss+ss2,ss.add_lookup(ss2)))
-        print("")
+ss = special_set(7)
+ss2 = special_set(7)
+# ss2 = special_set(7)
+print(ss+ss2)
+print(ss.add_lookup(ss2))
